1767cfdce1462019e87cf3e6e41f9c7f23ba5465
Tanner Brown <browntan2003@gmail.com>
2025-10-27 17:08:36 -0500

    wrote basic spring code including beans and components

--- src/main/java/com/example/ConnectToDBWithSpring/ConnectToDbWithSpringApplication.java
+++ src/main/java/com/example/ConnectToDBWithSpring/ConnectToDbWithSpringApplication.java
@@ -2,12 +2,31 @@ package com.example.ConnectToDBWithSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class ConnectToDbWithSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConnectToDbWithSpringApplication.class, args);
+		ApplicationContext context = SpringApplication.run(ConnectToDbWithSpringApplication.class, args);
+		DBConnection conn = context.getBean(DBConnection.class);
+		conn.accessEmailsTable();
+
+	}
+
+	/**
+	 * can return null if exception occurs
+	 * @return
+	 */
+	@Bean
+	public Connection getConnection() throws SQLException {
+		return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:free", "c##browntj16", "Foofighters95");
+
 	}
 
 }
--- src/main/java/com/example/ConnectToDBWithSpring/DBConnection.java
+++ src/main/java/com/example/ConnectToDBWithSpring/DBConnection.java
@@ -6,11 +6,29 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DBConnection {
-    private String userName;
-    private String password;
+    private Connection connection;
 
-    public DBConnection(String userName, String password){
-        this.userName = userName;
-        this.password = password;
+    public DBConnection(Connection connection){
+        this.connection = connection;
     }
+
+    /**
+     * Since I'm just testing with Spring I'm just reusing a table from a previous project. Anyways, I just want to make a basic
+     * function that grabs something from the database to test our connection.
+     */
+    public void accessEmailsTable(){
+        try{
+            Statement stmnt = connection.createStatement();
+            ResultSet rs = stmnt.executeQuery("SELECT email FROM sys.emails");
+            while(rs.next()){
+                System.out.println(rs.getString("email"));
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
+        }
+
+
+    }
+
+
 }
